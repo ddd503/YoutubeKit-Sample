08f183b2e128e95f0c31010faef261fe4158aab5
ddd503 <skbsporta7@gmail.com>
2019-03-28 01:35:29 +0900

    Initial Commit

---
+++ YoutubeKit-Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YoutubeKit-Sample
+//
+//  Created by kawaharadai on 2019/03/28.
+//  Copyright © 2019 kawaharadai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
