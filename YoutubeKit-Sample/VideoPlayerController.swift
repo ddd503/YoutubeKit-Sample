b5aef7951ca5035ab86c6a90db42d7948c727602
ddd503 <skbsporta7@gmail.com>
2019-03-30 12:31:04 +0900

    add base YTSwiftyPlayer

--- YoutubeKit-Sample/VideoPlayerController.swift
+++ YoutubeKit-Sample/VideoPlayerController.swift
@@ -7,5 +7,33 @@
 //
 
 import UIKit
+import YoutubeKit
 
-final class VideoPlayerController: UIViewController {}
+final class VideoPlayerController: UIViewController {
+
+    private var player: YTSwiftyPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Create a new player
+        player = YTSwiftyPlayer(
+            frame: CGRect(x: 0, y: 0, width: 640, height: 480),
+            playerVars: [.videoID("videoID-abcde")])
+
+        // Enable auto playback when video is loaded
+        player.autoplay = true
+
+        // Set player view.
+        view = player
+
+        // Set delegate for detect callback information from the player.
+        player.delegate = self
+
+        // Load the video.
+        player.loadPlayer()
+    }
+
+}
+
+extension VideoPlayerController: YTSwiftyPlayerDelegate {}
