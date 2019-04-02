b276851c96558289f7315d2431675e8e996d8d36
ddd503 <skbsporta7@gmail.com>
2019-04-03 02:31:29 +0900

    add extention for image view

--- YoutubeKit-Sample/VideoListVC/View/VideoListViewCell.swift
+++ YoutubeKit-Sample/VideoListVC/View/VideoListViewCell.swift
@@ -32,36 +32,3 @@ final class VideoListViewCell: UITableViewCell {
     }
     
 }
-
-extension UIImageView {
-    func setImageAsync(urlString: String?) {
-        guard let urlString = urlString, let url = URL(string: urlString) else {
-            DispatchQueue.main.async { [weak self] in
-                self?.image = UIImage(named: "no_image")!
-            }
-            return
-        }
-
-        let config = URLSessionConfiguration.default
-        config.requestCachePolicy = .returnCacheDataElseLoad
-
-        let session = URLSession(configuration: config)
-
-        session.dataTask(with: url) { [weak self] (data, response, error) in
-            var image: UIImage
-
-            if let error = error {
-                print(error.localizedDescription)
-                image = UIImage(named: "no_image")!
-            } else if let data = data, let downLoadImage = UIImage(data: data) {
-                image = downLoadImage
-            } else {
-                image = UIImage(named: "no_image")!
-            }
-
-            DispatchQueue.main.async { [weak self] in
-                self?.image = image
-            }
-            }.resume()
-    }
-}
